adbcadb2fd4458390aa4fa48abaf31ff59076826
iia <kha2464@gmail.com>
2022-02-16 09:29:19 +0900

    3. Long Function: 9. Preserve Whole Object
    객체 통째로 넘기기
    • 어떤 한 레코드에서 구할 수 있는 여러 값들을 함수에 전달하는 경우, 해당 매개변수를 레코드 하나로 교체할 수 있다.
    • 매개변수 목록을 줄일 수 있다. (향후에 추가할지도 모를 매개변수까지도…)
    • 이 기술을 적용하기 전에 의존성을 고려해야 한다.
    • 어쩌면 해당 메소드의 위치가 적절하지 않을 수도 있다. (기능 편애 “Feature Envy” 냄새에 해당한다.)

--- src/main/java/me/whiteship/refactoring/_03_long_function/_09_preserve_whole_object/Participant.java
+++ src/main/java/me/whiteship/refactoring/_03_long_function/_09_preserve_whole_object/Participant.java
@@ -12,5 +12,13 @@ public record Participant(String username, Map<Integer, Boolean> homework) {
         this.homework.put(index, true);
     }
 
-
+    // 인텔리제이 refactor - move instance method 기능을 이용해 연관이 있는 Participant 메소드로 이동시킴
+    // 물론 내마음처럼 찰떡같이 해주지는 않으니 옮겨놓고 조정 필요
+    // 이 메서드가 과연 그 클래스에 의존하는게 맞는가? 이 위치(원래 StudyDashboard에 있었음)에 있는게 맞는가 대한 고민을 하다가 Participant 로 옮김
+    double getRate(int totalNumberOfEvents) {
+        long count = homework().values().stream()
+                .filter(v -> v == true)
+                .count();
+        return (double) (count * 100 / totalNumberOfEvents);
+    }
 }
