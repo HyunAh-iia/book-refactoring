0682a12bfbb49465f7c5430bcc1cb736075899b7
hannah <hannah@taling.me>
2022-03-11 08:29:33 +0900

    6. Mutable Data: 18. Split Variable
    리팩토링 18. 변수 쪼개기 Split Variable
    • 어떤 변수가 여러번 재할당 되어도 적절한 경우
      • 반복문에서 순회하는데 사용하는 변수 또는 인덱스
      • 값을 축적시키는데 사용하는 변수
    • 그밖에 경우에 재할당 되는 변수가 있다면 해당 변수는 여러 용도로 사용되는 것이며 변수를 분리해야 더 이해하기 좋은 코드를 만들 수 있다.
      • 변수 하나 당 하나의 책임(Responsibility)을 지도록 만든다.
      • 상수를 활용하자. (자바스크립트의 const, 자바의 final)

--- src/main/java/me/whiteship/refactoring/_06_mutable_data/_18_split_variable/Haggis.java
+++ src/main/java/me/whiteship/refactoring/_06_mutable_data/_18_split_variable/Haggis.java
@@ -15,16 +15,16 @@ public class Haggis {
     }
 
     public double distanceTravelled(int time) {
-        double result;
-        double acc = primaryForce / mass;
-        int primaryTime = Math.min(time, delay);
-        result = 0.5 * acc * primaryTime * primaryTime;
+        double result; // 값을 누적시키기에 사용하므로 변수를 나눌 필요 없음!
+        final double primaryAcceleration = primaryForce / mass; // 가속도 = 추진력/질량
+        final int primaryTime = Math.min(time, delay); // 첫번째 추진력으로 이동한 시간
+        result = 0.5 * primaryAcceleration * primaryTime * primaryTime; // 첫번째 추진력으로 이동한 거리
 
-        int secondaryTime = time - delay;
+        final int secondaryTime = time - delay; // 두번째 추진력으로 이동한 시간
         if (secondaryTime > 0) {
-            double primaryVelocity = acc * delay;
-            acc = (primaryForce + secondaryForce) / mass;
-            result += primaryVelocity * secondaryTime + 0.5 * acc * secondaryTime + secondaryTime;
+            final double primaryVelocity = primaryAcceleration * delay; // 속도
+            final double secondaryAcceleration = (primaryForce + secondaryForce) / mass; // 가속도
+            result += primaryVelocity * secondaryTime + 0.5 * secondaryAcceleration * secondaryTime + secondaryTime; // 두번째 추진력으로 이동한 거리
         }
 
         return result;
