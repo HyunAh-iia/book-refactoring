0217cf0af73fde618d8261871b4039aca4f3389d
iia <kha2464@gmail.com>
2022-03-17 08:35:35 +0900

    6. Mutable Data: 23. Change Reference to Value [refactor]
    값 객체를 만들 때 Java 14에 나온 Record를 사용히도록 리팩토링

--- src/main/java/me/whiteship/refactoring/_06_mutable_data/_23_change_reference_to_value/TelephoneNumber.java
+++ src/main/java/me/whiteship/refactoring/_06_mutable_data/_23_change_reference_to_value/TelephoneNumber.java
@@ -1,36 +1,5 @@
 package me.whiteship.refactoring._06_mutable_data._23_change_reference_to_value;
 
-import java.util.Objects;
-
-public class TelephoneNumber {
-
-    private final String areaCode;
-
-    private final String number;
-
-    public TelephoneNumber(String areaCode, String number) {
-        this.areaCode = areaCode;
-        this.number = number;
-    }
-
-    public String areaCode() {
-        return areaCode;
-    }
-
-    public String number() {
-        return number;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        TelephoneNumber that = (TelephoneNumber) o;
-        return Objects.equals(areaCode, that.areaCode) && Objects.equals(number, that.number);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(areaCode, number);
-    }
+// Java 14 이후부터는 Record를 사용하면 된다 (17이 LTS 됐으니까 토이프로젝트에서 해보면 좋겠다)
+public record TelephoneNumber(String areaCode, String number) {
 }
