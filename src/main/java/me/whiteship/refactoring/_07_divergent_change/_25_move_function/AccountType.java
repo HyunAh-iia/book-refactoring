3913b82d10d4010d06ba583236968086f7740e01
iia <kha2464@gmail.com>
2022-03-21 09:18:14 +0900

    7. Divergent Change: 25. Move Function
    리팩토링 25. 함수 옮기기 Move Function
    • 모듈화가 잘 된 소프트웨어는 최소한의 지식만으로 프로그램을 변경할 수 있다.
    • 관련있는 함수나 필드가 모여있어야 더 쉽게 찾고 이해할 수 있다.
    • 하지만 관련있는 함수나 필드가 항상 고정적인 것은 아니기 때문에 때에 따라 옮겨야 할 필요가 있다.
    • 함수를 옮겨야 하는 경우
      • 해당 함수가 다른 문맥 (클래스)에 있는 데이터 (필드)를 더 많이 참조하는 경우.
      • 해당 함수를 다른 클라이언트 (클래스)에서도 필요로 하는 경우.
    • 함수를 옮겨갈 새로운 문맥 (클래스)이 필요한 경우에는 “여러 함수를 클래스로 묶기 (Combine Functions info Class)” 또는 “클래스 추출하기 (Extract Class)”를 사용한다.
    • 함수를 옮길 적당한 위치를 찾기가 어렵다면, 그대로 두어도 괜찮다. 언제든 나중에 옮길 수 있다.

--- src/main/java/me/whiteship/refactoring/_07_divergent_change/_25_move_function/AccountType.java
+++ src/main/java/me/whiteship/refactoring/_07_divergent_change/_25_move_function/AccountType.java
@@ -10,4 +10,20 @@ public class AccountType {
     public boolean isPremium() {
         return this.premium;
     }
+
+    // 책에서는 Account를 통째로 넘겨주는 예제도 존재함
+    // 그렇게 되면 Account에 있는 데이터를 많이 참조하게 되는 것
+    // 그러면 Account에 위치시키는게 타당하지 않을까 싶어서 백기선님은 매개변수로 필요한 것만 넘겨주는 것을 추천한다고 함
+    protected double overdraftCharge(int daysOverdrawn) {
+        if (isPremium()) {
+            final int baseCharge = 10;
+            if (daysOverdrawn <= 7) {
+                return baseCharge;
+            } else {
+                return baseCharge + (daysOverdrawn - 7) * 0.85;
+            }
+        } else {
+            return daysOverdrawn * 1.75;
+        }
+    }
 }
